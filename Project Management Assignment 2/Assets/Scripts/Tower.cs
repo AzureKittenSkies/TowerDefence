0fe6faf57437f102b20bbd0224741140241a3c84
AzureSkies <azzhayman@gmail.com>
2018-05-18 15:19:54 +1000

    Creation of Tower Defence repo and project

---
+++ Project Management Assignment 2/Assets/Scripts/Tower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    public int towerLevel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
